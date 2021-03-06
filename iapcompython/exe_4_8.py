135d7013b0e21c4dcbcf19fa4e6a382957185326
Edmilson Libanio Paulo <67564575+edmilsonlibanio@users.noreply.github.com>
2021-03-06 11:45:07 -0300

    Atualização de Exercícios
    Atualização de Exercícios

---
+++ iapcompython/exe_4_8.py
@@ -0,0 +1,2 @@
+# Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar. Você deve poder calcular soma (+), subtração (-), divisão (/) e multiplicação (*). Exiba o resultado da operação realizada.
+
